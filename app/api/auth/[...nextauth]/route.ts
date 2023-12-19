96ad0bb810111c2c6359d02f86d0c23effe36bff
Garland Key <gkey@valcom.com>
2023-12-19 00:13:06 -0500

    #10 - Removed prisma adapter as it's no longer necessary.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,19 +7,16 @@
 import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 import DiscordProvider from 'next-auth/providers/discord';
-// import { PrismaAdapter } from '@auth/prisma-adapter';
-// import { prisma } from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { session } from '@/lib/session';
-// import { User as AdapterUser } from '@prisma/client';
 
 const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const DISCORD_CLIENT_SECRET = process.env.DISCORD_CLIENT_SECRET;
 
 const authOptions: NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma),
-  // session: {
-  //   strategy: 'jwt',
-  // },
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     DiscordProvider({
       clientId: String(DISCORD_CLIENT_ID),
@@ -27,44 +24,43 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async signIn({ user, account, profile, credentials }): Promise<boolean> {
-      // if (account?.provider !== 'discord' && !user.id) {
-      //   throw new Error('No profile returned from Discord');
-      // }
-      // await prisma.user.upsert({
-      //   where: { discordId: (user as AdapterUser).discordId },
-      //   create: {
-      //     id: (user as AdapterUser).id,
-      //     discordId: (user as AdapterUser).discordId,
-      //     username: (user as AdapterUser).username,
-      //     avatar: (user as AdapterUser).avatar,
-      //   },
-      //   update: {
-      //     discordId: (user as AdapterUser).discordId,
-      //     username: (user as AdapterUser).username,
-      //     avatar: (user as AdapterUser).avatar,
-      //   },
-      // });
+    async signIn({ user, account }): Promise<boolean> {
+      if (account?.provider !== 'discord' && !user.id) {
+        throw new Error('No profile returned from Discord');
+      }
+      await prisma.user.upsert({
+        where: { email: user.email as string },
+        create: {
+          email: user.email as string,
+          name: user.name as string,
+          image: user.image as string,
+          id: user.id as string,
+        },
+        update: {
+          name: user.name as string,
+          image: user.image as string,
+        },
+      });
       return true;
     },
     session,
-    // async jwt({ token, user, account, profile }) {
-    //   if (profile) {
-    //     try {
-    //       const user: AdapterUser | null = await prisma.user.findUnique({
-    //         where: { discordId: (profile as AdapterUser).discordId },
-    //       });
-    //       if (!user) {
-    //         throw new Error('No user found');
-    //       }
-    //       token.id = user.id;
-    //     } catch (error) {
-    //       console.log('Error finding user:', error);
-    //       throw new Error('Failed to find user.');
-    //     }
-    //   }
-    //   return token;
-    // },
+    async jwt({ token, user }) {
+      if (user) {
+        try {
+          const userAccount = await prisma.user.findUnique({
+            where: { email: user.email as string },
+          });
+          if (!user) {
+            throw new Error('No user found');
+          }
+          token.id = user.id;
+        } catch (error) {
+          console.log('Error finding user:', error);
+          throw new Error('Failed to find user.');
+        }
+      }
+      return token;
+    },
   },
 };
 const handler = NextAuth(authOptions);

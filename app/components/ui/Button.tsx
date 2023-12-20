8eaf39066ccd3ef47b8658b18b2dae24289251ab
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2023-12-20 23:30:20 +0900

    #19 Add props title and href; Add styles

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -4,14 +4,18 @@ import Link from 'next/link';
 
 interface ButtonProps {
   title: string;
+  href: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ title }) => {
+const Button: React.FC<ButtonProps> = ({ title, href }) => {
   return (
-    <Link href="/app/api/auth/signin">
-      <a className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+    <Link href={href}
+        className="bg-violet-600 hover:bg-violet-700 
+        text-white font-bold py-3 px-8 rounded-full 
+        bg-opacity-75 text-lg">
+
         {title}
-      </a>
+        
     </Link>
   );
 };

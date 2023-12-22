0934bfc7bf2beccd1f82c891bd95d818a075f770
sujade <dnmzsu@gmail.com>
2023-12-22 13:03:20 +0100

    #32 added a background container and footer image.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'footer-texture': "url('/footerbg.svg')"
       },
     },
   },

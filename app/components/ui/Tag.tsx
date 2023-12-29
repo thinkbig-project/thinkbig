5e88a248bde44b63f360c1001c6268bb065e3a69
Julien Mellon <mellon.julien@gmail.com>
2023-12-29 12:42:31 -0500

    fix: playing with tag colors

--- app/components/ui/Tag.tsx
+++ app/components/ui/Tag.tsx
@@ -7,35 +7,31 @@ export interface TagProps {
 //TODO limit type prop to "css" | "ejs" | "html" | "js" | "mongo/mongoose" | "node/express" | "react"
 
 export default function Tag({type}: TagProps) {
-    console.log('loading tag', type)
-    
 
     let tagStyle : string = ''
-    console.log(tagStyle)
 
     switch (type) {
         case 'css':
-          tagStyle = 'bg-blue-300 text-white';
+          tagStyle = 'bg-[#0959df] text-white';
           break;
         case 'ejs':
-          tagStyle = 'bg-red-300 text-black'
+          tagStyle = 'bg-[#dfdd09] text-black'
           break;
         case 'html':
           tagStyle = 'bg-[#2d2b6b] text-black'
           break;
         case 'mongo':
-          tagStyle = 'bg-green-700 text-white'
+          tagStyle = 'bg-[#00c55b] text-white'
           break;
         case 'node':
-          tagStyle = 'bg-green-300 text-black'
+          tagStyle = 'bg-[#6aa05c] text-black'
           break;
         case 'react':
-          tagStyle = 'bg-blue-400 text-white'
+          tagStyle = 'bg-[#5fdafb] text-white'
           break;  
         default:
           tagStyle = 'bg-yellow-400 text-black'
       }
-    console.log('swtich applied', tagStyle)
     
     const mainTagStyle = 'px-2 py-1 rounded font-bold'
 

5ab85d325312aaa3c086bf0f5914e9b92830323d
Garland Key <gkey@valcom.com>
2024-01-03 18:42:31 -0500

    #38 Card component layout added

--- app/components/ui/Avatar.tsx
+++ app/components/ui/Avatar.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image';
 
 export interface AvatarProps {
   avatar: string;
+  className?: string;
 }
 
-export default function Avatar({ avatar }: AvatarProps) {
+export default function Avatar({ avatar, className }: AvatarProps) {
   return (
     <Image
-      className='aspect-square rounded-full hover:brightness-110'
+      className={`aspect-square rounded-full hover:brightness-110 ${className}`}
       src={avatar}
       alt='Profile picture of the user'
       height={48}
--- app/components/ui/Card.tsx
+++ app/components/ui/Card.tsx
@@ -20,24 +20,35 @@ const handleClick = () => {
 
 const Card: React.FC<CardProps> = ({ type, title, desc, matches }) => {
   return (
-    <section className='w-1/2'>
-      <div>
-        <Image src='/placeholder.png' alt='type' height={50} width={50} />
-        <h2>{title}</h2>
-        <div>
-          <Image src='/placeholder.png' alt='type' height={50} width={50} />
-          <Image src='/placeholder.png' alt='type' height={50} width={50} />
+    <>
+      <section className='bg-indigo-950 flex flex-col p-4 rounded-3xl w-1/2'>
+        <div className='flex flex-row gap-4 h-12 items-center w-full'>
+          <Image src='/placeholder.png' alt='type' height={40} width={40} />
+          <h2 className='text-2xl w-full'>{title}</h2>
+          <div className='flex flex-row gap-2 justify-end w-full'>
+            <Image src='/placeholder.png' alt='type' height={32} width={32} />
+            <Image src='/placeholder.png' alt='type' height={32} width={32} />
+          </div>
         </div>
-      </div>
-      <div>
-        <p>{desc}</p>
-        <div>
-          {matches.map((match) => (
-            <Avatar key={match.id} avatar={match.avatar} />
-          ))}
+
+        <div className='flex flex-row items-center'>
+          <p className='bg-gradient-to-r from-indigo-900 p-2 rounded-lg truncate w-3/4'>
+            {desc}
+          </p>
+          <div className='flex flex-row justify-end w-1/4'>
+            {matches.map(match => {
+              return (
+                <Avatar
+                  key={match.id}
+                  avatar={match.avatar}
+                  className={`z-10 -ml-[1.5rem] border-[0.175rem] border-indigo-950`}
+                />
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 

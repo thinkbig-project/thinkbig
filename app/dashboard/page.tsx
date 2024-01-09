54e13814259c694a8f14fb28ee63b3787ecb9f32
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-01-09 22:45:29 +0900

    Place button in dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,13 @@
 import IssueIcon from "../components/ui/IssueIcon";
+import CloseButton from "../components/ui/CloseButton";
+
 
 const Dashboard = async () => {
+
   return (
     <>
       <h1>Dashboard</h1>
+      <CloseButton />
       <div>
         <IssueIcon type="assignment" />
         <IssueIcon type="class" />

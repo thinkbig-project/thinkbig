d2e862d0d24a1c58a3f03f8a0f4c35f97efe1899
Garland Key <gkey@valcom.com>
2024-01-17 19:22:52 -0500

    adjust colors in dashboard and sidenav

--- app/components/nav/SideNav.tsx
+++ app/components/nav/SideNav.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const SideNav: React.FC = () => {
   return (
-    <div className='bg-neutral-950 flex flex-col h-screen items-center justify-between py-4 sticky text-white top-0 w-20'>
+    <div className='bg-slate-900 flex flex-col h-screen items-center justify-between py-4 sticky text-white top-0 w-20'>
       <div className='content-start flex flex-col gap-4 h-full'>
         {/* nav buttons here */}
       </div>
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import SideNav from '@/app/components/nav/SideNav';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='bg-slate-900 flex flex-row h-screen w-screen'>
+    <div className='bg-slate-950 flex flex-row h-screen w-screen'>
       <SideNav />
       {children}
     </div>

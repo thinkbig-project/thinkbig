e3f05ff94034e04d627487fe2e9d187a9fae1388
Doug-Colin <dougcolin@gmail.com>
2024-02-25 18:18:44 -0500

    77 Updated Button Component

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,18 +1,35 @@
-import Link from 'next/link';
+'use client'
 
+import Link from 'next/link';
 import { ButtonProps } from '@/types'; // type/interface props
 
-const Button: React.FC<ButtonProps> = ({ title, href }) => {
+
+
+const Button: React.FC<ButtonProps> = ({ title, href, type = 'button' }) => {
+
+  const buttonClasses = `bg-violet-600 hover:bg-violet-700 
+  text-white font-bold py-3 px-8 rounded-full 
+  bg-opacity-75 text-lg`
+
+  // If href is provided, render button as Next.js Link.
+  if (href) {
+    return (
+      <Link href={href}
+        className={buttonClasses}>
+        {title}
+      </Link>
+    );
+  }
+
+  // Otherwise, return button with type specified by props.
   return (
-    <Link href={href}
-      className="bg-violet-600 hover:bg-violet-700 
-        text-white font-bold py-3 px-8 rounded-full 
-        bg-opacity-75 text-lg">
-
+    <button
+      type={type}
+      className={buttonClasses}
+    >
       {title}
-
-    </Link>
-  );
+    </button>
+  )
 };
 
 export default Button; 
